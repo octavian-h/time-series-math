48198d38c3f1a0f56a9e1378dc71b5bbde7af353
Octavian <h.octavian@gmail.com>
2015-07-17 15:54:25 +0300

    [start-release] test

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,12 +4,23 @@ COMMIT_MESSAGE="$(git log --format=%B -n 1 $TRAVIS_COMMIT)"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
    [ "$TRAVIS_TAG" == "" ] &&
-   [[ "$COMMIT_MESSAGE" != *"[no-deploy]"* ]] &&
-   [[ "$COMMIT_MESSAGE" != "[maven-release-plugin]"* ]] &&
-   [[ "$COMMIT_MESSAGE" != *"[start-release]"* ]]; then
+   [[ "$COMMIT_MESSAGE" != "[maven-release-plugin]"* ]]; then
 
-   echo "Deploy Snapshot artifacts"
-   mvn deploy -DskipTests=true --settings ci/maven-settings.xml
+    if [[ "$COMMIT_MESSAGE" != *"[no-deploy]"* ]] &&
+       [[ "$COMMIT_MESSAGE" != *"[start-release]"* ]]; then
+
+        echo "Deploy Snapshot artifacts"
+        mvn --batch-mode deploy -DskipTests=true --settings ci/maven-settings.xml
+
+    elif [[ "$COMMIT_MESSAGE" == *"[start-release]"* ]]; then
+
+        echo "Release artifacts"
+        mvn --batch-mode release:prepare --settings ci/maven-settings.xml
+        mvn --batch-mode release:perform --settings ci/maven-settings.xml
+
+    else
+        echo "Skip deploy/release"
+    fi
 else
-   echo "Skip deploy"
+   echo "Skip deploy/release"
 fi
