43a96a3a50c6b456b8dde33af0ab1abd5ff40c5a
Octavian <h.octavian@gmail.com>
2015-07-17 23:12:41 +0300

    add DFT

---
+++ src/main/java/ro/hasna/ts/math/representation/DiscreteFourierTransform.java
@@ -0,0 +1,57 @@
+package ro.hasna.ts.math.representation;
+
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.transform.DftNormalization;
+import org.apache.commons.math3.transform.FastFourierTransformer;
+import org.apache.commons.math3.transform.TransformType;
+
+/**
+ * Implements the Discrete Fourier Transformation using {@link FastFourierTransformer}.
+ *
+ * @since 1.0
+ */
+public class DiscreteFourierTransform {
+    private FastFourierTransformer fourierTransformer;
+
+    public DiscreteFourierTransform() {
+        this(new FastFourierTransformer(DftNormalization.STANDARD));
+    }
+
+    public DiscreteFourierTransform(FastFourierTransformer fourierTransformer) {
+        this.fourierTransformer = fourierTransformer;
+    }
+
+    /**
+     * Transform a given sequence of values into Fourier coefficients using {@link FastFourierTransformer}.
+     * The sequence is padded with zeros if it hasn't the right length.
+     *
+     * @param values the sequence of values
+     * @return the result of the transformation
+     */
+    public double[] transformToDoubleArray(double[] values) {
+        // pad the input array with zeros so as to have a length == 2^k
+        int initialLength = values.length;
+        int powerOfTwo = Integer.highestOneBit(initialLength);
+        if (initialLength != powerOfTwo) {
+            powerOfTwo = powerOfTwo << 1;
+        }
+        double[] copy = new double[powerOfTwo];
+        System.arraycopy(values, 0, copy, 0, initialLength);
+        for (int i = initialLength; i < powerOfTwo; i++) {
+            copy[i] = 0;
+        }
+
+        // run FFT
+        Complex[] complexes = fourierTransformer.transform(copy, TransformType.FORWARD);
+
+        // keep only the most important coefficients
+        int outputLength = (powerOfTwo >> 1) + 1;
+        double[] result = new double[outputLength];
+        for (int i = 0; i < outputLength && i < complexes.length; i++) {
+            // multiply the values with 2/N
+            result[i] = complexes[i].multiply(2).divide(initialLength).abs();
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/ro/hasna/ts/math/representation/DiscreteFourierTransformTest.java
@@ -0,0 +1,89 @@
+package ro.hasna.ts.math.representation;
+
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.transform.FastFourierTransformer;
+import org.apache.commons.math3.transform.TransformType;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import ro.hasna.ts.math.util.TimeSeriesPrecision;
+
+/**
+ * @since 1.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DiscreteFourierTransformTest {
+
+    @InjectMocks
+    private DiscreteFourierTransform discreteFourierTransform;
+
+    @Mock
+    private FastFourierTransformer fastFourierTransformer;
+
+    @Before
+    public void setUp() throws Exception {
+        Mockito.when(fastFourierTransformer.transform(Mockito.<double[]>any(), Mockito.<TransformType>any())).thenReturn(new Complex[]{});
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        discreteFourierTransform = null;
+        fastFourierTransformer = null;
+    }
+
+    @Test
+    public void testTransform() throws Exception {
+        double[] v = {1, 2, 3};
+        discreteFourierTransform.transformToDoubleArray(v);
+
+
+        Mockito.verify(fastFourierTransformer).transform(new double[]{1, 2, 3, 0}, TransformType.FORWARD);
+    }
+
+    @Test
+    public void testTransformPowerOfTwo() throws Exception {
+        double[] v = {1, 2, 3, 4};
+        discreteFourierTransform.transformToDoubleArray(v);
+
+        Mockito.verify(fastFourierTransformer).transform(v, TransformType.FORWARD);
+    }
+
+    @Test
+    public void testTransformSineWave() throws Exception {
+        double signalFrequency = 100;
+        double amplitude = 30;
+        double displacement = 17;
+        double samplingFrequency = signalFrequency * 8; //it should be at least double (Shannon Theorem)
+        int len = 1000;
+        double[] v = new double[len];
+        for (int i = 0; i < len; i++) {
+            v[i] = amplitude * Math.sin(2 * Math.PI * signalFrequency * i / samplingFrequency) + displacement;
+        }
+
+        double[] result = new DiscreteFourierTransform().transformToDoubleArray(v);
+
+        double max = 0;
+        int pos = 0;
+        for (int i = 1; i < result.length; i++) {
+            if (max < result[i]) {
+                max = result[i];
+                pos = i;
+            }
+        }
+
+        int powerOfTwo = Integer.highestOneBit(len);
+        if (len != powerOfTwo) {
+            powerOfTwo = powerOfTwo << 1;
+        }
+
+        Assert.assertEquals(amplitude, max, TimeSeriesPrecision.EPSILON);
+        Assert.assertEquals(signalFrequency, pos * samplingFrequency / powerOfTwo, TimeSeriesPrecision.EPSILON);
+        Assert.assertEquals(displacement, result[0] / 2, TimeSeriesPrecision.EPSILON);
+    }
+}
