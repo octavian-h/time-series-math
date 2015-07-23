be4caa7d8974452b891c41d03623ef6b53b7f1e2
Octavian <h.octavian@gmail.com>
2015-07-23 13:24:51 +0300

    add adapter for EuclideanDistance

---
+++ src/main/java/ro/hasna/ts/math/ml/distance/EuclideanDistanceAdapter.java
@@ -0,0 +1,31 @@
+package ro.hasna.ts.math.ml.distance;
+
+import org.apache.commons.math3.util.FastMath;
+
+/**
+ * Adapter class for {@link org.apache.commons.math3.ml.distance.EuclideanDistance} that
+ * implements {@link GenericDistanceMeasure}.
+ *
+ * @since 1.0
+ */
+public class EuclideanDistanceAdapter implements GenericDistanceMeasure<double[]> {
+
+    @Override
+    public double compute(double[] a, double[] b) {
+        return compute(a, b, Double.POSITIVE_INFINITY);
+    }
+
+    @Override
+    public double compute(double[] a, double[] b, double cutOffValue) {
+        double sum = 0.0;
+        double transformedCutoff = cutOffValue * cutOffValue;
+        for (int i = 0; i < a.length; i++) {
+            double aux = a[i] - b[i];
+            sum += aux * aux;
+            if (sum > transformedCutoff) {
+                return Double.POSITIVE_INFINITY;
+            }
+        }
+        return FastMath.sqrt(sum);
+    }
+}
---
+++ src/test/java/ro/hasna/ts/math/ml/distance/EuclideanDistanceAdapterTest.java
@@ -0,0 +1,76 @@
+package ro.hasna.ts.math.ml.distance;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ro.hasna.ts.math.util.TimeSeriesPrecision;
+
+/**
+ * @since 1.0
+ */
+public class EuclideanDistanceAdapterTest {
+    private EuclideanDistanceAdapter distance;
+
+    @Before
+    public void setUp() throws Exception {
+        distance = new EuclideanDistanceAdapter();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        distance = null;
+    }
+
+    @Test
+    public void testTriangleInequality() throws Exception {
+        int n = 128;
+        double a[] = new double[n];
+        double b[] = new double[n];
+        double c[] = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = i;
+            b[i] = n - i;
+            c[i] = i * i;
+        }
+
+        double ab = distance.compute(a, b);
+        double ba = distance.compute(b, a);
+        double bc = distance.compute(b, c);
+        double ac = distance.compute(a, c);
+
+        Assert.assertEquals(ab, ba, TimeSeriesPrecision.EPSILON);
+        Assert.assertTrue(ab + bc >= ac);
+    }
+
+    @Test
+    public void testEquality() throws Exception {
+        int n = 128;
+        double a[] = new double[n];
+        double b[] = new double[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i;
+            b[i] = i;
+        }
+
+        double result = distance.compute(a, b);
+
+        Assert.assertEquals(0, result, TimeSeriesPrecision.EPSILON);
+    }
+
+    @Test
+    public void testOverflow() throws Exception {
+        int n = 128;
+        double a[] = new double[n];
+        double b[] = new double[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i;
+            b[i] = i + 100;
+        }
+
+        double result = distance.compute(a, b, 100);
+
+        Assert.assertEquals(Double.POSITIVE_INFINITY, result, TimeSeriesPrecision.EPSILON);
+    }
+}
