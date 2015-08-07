99469303cb46fd32418235d30c528ac960d0deb7
Octavian <h.octavian@gmail.com>
2015-08-07 16:53:10 +0300

    improve space complexity of DTW

--- src/main/java/ro/hasna/ts/math/ml/distance/DynamicTimeWarping.java
+++ src/main/java/ro/hasna/ts/math/ml/distance/DynamicTimeWarping.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2015 Octavian Hasna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ro.hasna.ts.math.ml.distance;
 
 import org.apache.commons.math3.util.FastMath;
@@ -12,7 +27,8 @@ import org.apache.commons.math3.util.FastMath;
  * @since 1.0
  */
 public class DynamicTimeWarping implements GenericDistanceMeasure<double[]> {
-    private int radius;
+    private static final long serialVersionUID = 1154818905340336905L;
+    private final int radius;
 
     public DynamicTimeWarping(int radius) {
         this.radius = radius;
@@ -32,11 +48,10 @@ public class DynamicTimeWarping implements GenericDistanceMeasure<double[]> {
     protected double computeFullDtw(double[] a, double[] b, double cutOffValue) {
         int n = a.length;
         int m = b.length;
-        double[][] d = new double[n + 1][m + 1];
+        double[][] d = new double[2][m + 1];
 
-        //TODO use only two arrays
         int w = FastMath.max(radius, FastMath.abs(n - m));
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < 2; i++) {
             for (int j = 0; j <= m; j++) {
                 d[i][j] = Double.POSITIVE_INFINITY;
             }
@@ -49,14 +64,18 @@ public class DynamicTimeWarping implements GenericDistanceMeasure<double[]> {
             int end = FastMath.min(m, i + w);
             for (int j = start; j <= end; j++) {
                 double aux = FastMath.abs(a[i - 1] - b[j - 1]);
-                d[i][j] = aux + FastMath.min(d[i - 1][j], FastMath.min(d[i][j - 1], d[i - 1][j - 1]));
-                if (d[i][j] > cutOffValue) {
+                d[1][j] = aux + FastMath.min(d[0][j], FastMath.min(d[1][j - 1], d[0][j - 1]));
+                if (d[1][j] > cutOffValue) {
                     return Double.POSITIVE_INFINITY;
                 }
             }
+            for (int j = 0; j <= m; j++) {
+                d[0][j] = d[1][j];
+                d[1][j] = Double.POSITIVE_INFINITY;
+            }
         }
 
-        return d[n][m];
+        return d[0][m];
     }
 
     @Override
