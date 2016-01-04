de792423b42ba4661ba8f0a1a3072470c7d164dc
Octavian <h.octavian@gmail.com>
2016-01-04 19:23:49 +0200

    update license year

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2015 Octavian Hasna
+# Copyright (C) 2016-2015 Octavian Hasna
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
