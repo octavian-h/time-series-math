b1e1c2f1c37fa7e3f306dc0587452d97414d7e1d
Octavian <h.octavian@gmail.com>
2020-01-11 21:18:38 +0200

    [start-release] update travis env

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -15,20 +15,13 @@
 # limitations under the License.
 #
 
-COMMIT_MESSAGE="$(git log --format=%B -n 1 "${TRAVIS_COMMIT}")"
+COMMIT_MESSAGE="$(git log --format=%s --max-count 1)"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
-   [ "$TRAVIS_TAG" == "" ] &&
-   [[ "$COMMIT_MESSAGE" != "[maven-release-plugin]"* ]]; then
+    [[ "$COMMIT_MESSAGE" != *"[no-deploy]"* ]] &&
+    [[ "$COMMIT_MESSAGE" != "[maven-release-plugin]"* ]]; then
 
-    if [[ "$COMMIT_MESSAGE" != *"[no-deploy]"* ]] &&
-       [[ "$COMMIT_MESSAGE" != *"[start-release]"* ]]; then
-
-        echo "Deploy Snapshot artifacts"
-        mvn --batch-mode deploy -DskipTests=true --settings ci/maven-settings.xml
-
-    elif [[ "$COMMIT_MESSAGE" == *"[start-release]"* ]]; then
-
+    if [[ "$COMMIT_MESSAGE" == *"[start-release]"* ]]; then
         echo "Release artifacts"
         # point HEAD to master branch (this is needed by the maven release plugin)
         git symbolic-ref HEAD refs/heads/master
@@ -46,9 +39,9 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
              --user octavian-h:"$BINTRAY_API_KEY" \
              --header "content-type: application/json" \
              --data "{\"username\": \"$SONATYPE_USER\",\"password\": \"$SONATYPE_TOKEN\",\"close\": \"1\"}"
-
     else
-        echo "Skip deploy/release"
+        echo "Deploy Snapshot artifacts"
+        mvn --batch-mode deploy -DskipTests=true --settings ci/maven-settings.xml
     fi
 else
    echo "Skip deploy/release"
